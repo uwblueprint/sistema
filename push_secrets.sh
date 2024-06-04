35f398c2a3578b614d8ce67451b0aedd7e164bd5
Chinemerem <chinemeremchigbo@Outlook.com>
2024-06-04 00:08:26 -0400

    delete all secrets and remake them

--- push_secrets.sh
+++ push_secrets.sh
@@ -3,31 +3,38 @@
 # Define the input file
 ENV_FILE=".env"
 
-# Ensure the Vault CLI is authenticated
-login_message=$(vlt login)
-if [[ "$login_message" != "Successfully logged in" ]]; then
-    echo "Currently not logged in. Please login to Vault first to set up the secrets."
-    exit 1
+# Login to Vault
+echo "Logging into Vault..."
+vlt login
+if [ $? -ne 0 ]; then
+echo "Failed to login to Vault. Please check your credentials."
+exit 1
 fi
 
-# Check if .env file exists
+vlt config init
+
+# Check if .env file exists and exit if it doesn't
 if [ ! -f "$ENV_FILE" ]; then
     echo "Error: $ENV_FILE not found."
     exit 1
 fi
 
-# Read the secrets from the .env file
+# Fetch all existing secret keys and delete them
+echo "Fetching and deleting all existing secrets..."
+SECRET_KEYS=$(vlt secrets list -format=json | grep -Eo '"([^"]*)"\s*:\s*"([^"]*)"' | sed -E 's/^"([^"]*)"\s*:\s*"([^"]*)"$/\1=\2/' | grep "^name=" | grep -v "@" | sed 's/^name=//')
+
+for secret_key in $SECRET_KEYS; do
+    echo "Deleting secret with name $secret_key"
+    vlt secrets delete "$secret_key" || echo "Failed to delete secret $secret_key."
+    echo ""
+done
+
+# Read the secrets from the .env file and create them
 while IFS='=' read -r key value; do
     if [ -n "$key" ] && [ -n "$value" ]; then
-        # Check if the secret already exists
-        secret_exists=$(vlt secrets get "$key" 2>&1)
-        if [[ "$secret_exists" == *"not found"* ]]; then
-            # Secret does not exist, create it
-            vlt secrets create "$key"="$value" || echo "Failed to create secret for $key."
-        else
-            # Secret exists, update it
-            vlt secrets update "$key"="$value" || echo "Failed to update secret for $key."
-        fi
+        # Create the secret with the new value
+        vlt secrets create "$key"="$value" || echo "Failed to create secret for $key."
+        echo ""
     fi
 done < "$ENV_FILE"
 
