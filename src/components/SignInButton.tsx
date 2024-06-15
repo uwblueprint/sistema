f156f350e2429f12ee22bc9998ebb41e4310bddb
Rahul Tandon <jesuisrahult@gmail.com>
2024-06-15 13:59:16 -0400

    change callback url

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -3,5 +3,5 @@
 import { signIn } from "next-auth/react"
  
 export function SignInButton() {
-  return <button onClick={() => signIn("google" , {callbackUrl: `${window.location.origin}/homepage`})}>Sign In Google</button>
+  return <button onClick={() => signIn("google" , {callbackUrl: `/homepage`})}>Sign In Google</button>
 }
