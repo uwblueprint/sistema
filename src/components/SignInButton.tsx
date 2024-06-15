9134992f7bdb55b1cd6f69166872494a13f50704
Rahul Tandon <jesuisrahult@gmail.com>
2024-06-15 13:02:14 -0400

    delete unnecessary code in SignInButton and route.ts

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -1,20 +1,7 @@
-// import { signInWithGoogle } from './serverActions';
-// import { signIn } from "auth";
-
-
-// export function SignInButton() {
-//   return (
-//       // <form method="post" action="./app/auth/[...nextauth]/route.ts">  
-//       //     <button type="submit">Sign in with Google</button>
-//       // </form>
-//       <button onClick={async () => {
-//         await signIn()
-//       }}>Hello World!</button>
-//   );
-// }
-
+// importing from next-auth/react instead of our root's auth.ts because of Client & Server component issues.
+// https://authjs.dev/getting-started/session-management/login
 import { signIn } from "next-auth/react"
  
 export function SignInButton() {
-  return <button onClick={() => signIn("google" , {callbackUrl: `${window.location.origin}/prevIndex`})}>Sign In Google</button>
+  return <button onClick={() => signIn("google" , {callbackUrl: `${window.location.origin}/homepage`})}>Sign In Google</button>
 }
