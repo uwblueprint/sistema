22ecf88b96834179cc3ba2a70f5c42e9b1be1793
Vihaan Jagiasi <vihaan@jagiasi.com>
2024-06-24 22:37:02 -0400

    Add seeding script

---
+++ prisma/seed/seed.config.ts
@@ -0,0 +1,17 @@
+import { SeedPrisma } from "@snaplet/seed/adapter-prisma";
+import { defineConfig } from "@snaplet/seed/config";
+import { PrismaClient } from "@prisma/client";
+
+export default defineConfig({
+  adapter: () => {
+    const client = new PrismaClient({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL,
+        },
+      },
+    });
+    return new SeedPrisma(client);
+  },
+  select: ["!*_prisma_migrations"],
+});
