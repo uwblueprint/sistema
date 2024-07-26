63e6e72c5cc1e4a1e86dcd0a53f45fd2fc62d32c
ludavidca <davidjylu7@gmail.com>
2024-07-25 22:40:18 -0400

    pnpm lock regenerated

--- prisma/seed/seed.config.ts
+++ prisma/seed/seed.config.ts
@@ -1,14 +1,13 @@
 import { SeedPrisma } from '@snaplet/seed/adapter-prisma';
 import { defineConfig } from '@snaplet/seed/config';
 import { PrismaClient } from '@prisma/client';
-import { db } from '@vercel/postgres';
 
 // export default defineConfig({
 //   adapter: () => {
 //     const client = new PrismaClient({
 //       datasources: {
 //         db: {
-//           // url: process.env.POSTGRES_DATABASE,
+//           url: process.env.DATABASE_URL,
 //         },@vercel/postgres
 //       },
 //     });
