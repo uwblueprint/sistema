4be7a379d953dc22fa9e43aeb9ebc8a4e50ac821
evelina <91142355+135ze@users.noreply.github.com>
2024-10-01 20:41:11 -0400

    add unstyled teacher profile page with gauth info and x/10 absences

---
+++ src/pages/profile.tsx
@@ -0,0 +1,63 @@
+import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+import { SignOutButton } from '../components/SignOutButton';
+
+export default function AnotherPage() {
+  const { data: session, status } = useSession();
+  const [numAbsences, setNumAbsences] = useState(null);
+  const [usedAbsences, setUsedAbsences] = useState(null);
+
+  useEffect(() => {
+    const fetchUserInfoByEmail = async () => {
+      if (!session || !session.user || !session.user.email) return;
+
+      const email = session.user.email;
+      const apiUrl = `/api/users/email/${email}?getAbsences=true`;
+
+      try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        const data = await response.json();
+        setNumAbsences(data['numOfAbsences']);
+        setUsedAbsences(data['absences'].length);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchUserInfoByEmail();
+  }, [session]);
+
+  return (
+    <div>
+      <h1>Profile</h1>
+      <hr />
+      {status === 'loading' ? (
+        <p>Loading...</p>
+      ) : session && session.user ? (
+        <div>
+          <h2>Personal Information</h2>
+          <p>Name: {session.user.name}</p>
+          <p>Email: {session.user.email}</p>
+          <p>Image:</p>
+          {session.user.image && (
+            <img src={session.user.image} alt="User Image" />
+          )}
+
+          <hr />
+          <h2>Metrics</h2>
+          <p>
+            Absences: {usedAbsences} / {numAbsences}{' '}
+          </p>
+          <hr />
+        </div>
+      ) : (
+        <p>Error: Signed out</p>
+      )}
+      <hr />
+      <SignOutButton />
+    </div>
+  );
+}
