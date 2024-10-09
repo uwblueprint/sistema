6bd0a9037a0b2e429426e392f4a1872401b4074e
Kenzy Soror <kenzy.soror.05@gmail.com>
2024-10-08 22:02:57 -0400

    Style login page

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -1,11 +1,23 @@
 // importing from next-auth/react instead of our root's auth.ts because of Client & Server component issues.
 // https://authjs.dev/getting-started/session-management/login
 import { signIn } from 'next-auth/react';
+import { Button } from '@chakra-ui/react';
+import { FcGoogle } from 'react-icons/fc';
 
 export function SignInButton() {
   return (
-    <button onClick={() => signIn('google', { callbackUrl: `/homepage` })}>
-      Sign In Google
-    </button>
+    <Button
+      variant="outline"
+      borderColor="blue.400"
+      colorScheme="gray"
+      width="275px"
+      height="45px"
+      color="black"
+      fontWeight="bold"
+      leftIcon={<FcGoogle fontSize="18px" />}
+      onClick={() => signIn('google', { callbackUrl: `/homepage` })}
+    >
+      Sign in with Google
+    </Button>
   );
 }
---
+++ src/components/SistemaLogoColour.tsx
@@ -0,0 +1,7 @@
+import { Image } from '@chakra-ui/react';
+
+export function SistemaLogoColour() {
+  return (
+    <Image src="/images/sistema_logo_colour.png" alt="Sistema Logo Colour" />
+  );
+}
