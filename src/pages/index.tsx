30064508ec3e9cfa78a2791b0c353e0363a9ef77
Kenzy Soror <kenzy.soror.05@gmail.com>
2024-10-19 17:59:54 -0400

    Upload refined logo, remove unnecessary comments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,7 @@ import { Box, Flex, Text } from '@chakra-ui/react';
 
 export default function Index() {
   return (
-    <Flex
-      direction="column" // stack items vertically
-      align="center" // center items horizontally
-      justify="center" // center items vertically
-      height="100vh" // make flex container take full viewport height
-    >
+    <Flex direction="column" align="center" justify="center" height="100vh">
       <Box width="280px" height="auto" mb="5">
         <SistemaLogoColour />
       </Box>
