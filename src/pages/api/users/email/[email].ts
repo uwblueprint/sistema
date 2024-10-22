8dc0cb0ddf9702de22ba870493994a4d16139219
evelina zheng <evelina.zheng@uwaterloo.ca>
2024-10-22 19:52:31 -0400

    Update src/pages/api/users/email/[email].ts
    Co-authored-by: Chinemerem <chinemeremchigbo@Outlook.com>

--- src/pages/api/users/email/[email].ts
+++ src/pages/api/users/email/[email].ts
@@ -20,33 +20,28 @@ export default async function handler(
   const shouldIncludeAbsences = params.shouldIncludeAbsences === 'true';
   const realEmail = params.email as string;
   try {
-    // TODO: Please see https://www.notion.so/uwblueprintexecs/Teacher-Profile-Popup-Frontend-0e847ff1bef04037afd2b4d104875afa?pvs=4
     const useFake = true;
-    let email;
+    let email: string;
     if (useFake) {
-      // random email from DB
       const randomUser = await getRandomUser();
       email = randomUser.email;
     } else {
       email = realEmail;
     }
 
-    // get the user from the DB
     const user = await prisma.user.findUniqueOrThrow({
       where: { email },
       include: { absences: shouldIncludeAbsences },
     });
 
-    res.status(200).json(user);
+    return res.status(200).json(user);
   } catch (error) {
-    // if the find first query (for finding a random user email) failed
     if (
       error instanceof Prisma.PrismaClientKnownRequestError &&
       error.code === 'P2025'
     ) {
-      res.status(400).json({ error: 'Database is empty or user not found.' });
+      return res.status(400).json({ error: 'Database is empty or user not found.' });
     }
-
-    res.status(500).json({ error: 'Internal server error' });
+    return res.status(500).json({ error: 'Internal server error' });
   }
 }
