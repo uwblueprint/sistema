27628a411f801db39cca495c59119a821b214004
evelina <91142355+135ze@users.noreply.github.com>
2024-10-22 21:46:41 -0400

    move /users and /users/[id] path to pages

--- app/api/users/route.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-import { NextResponse } from 'next/server';
-
-const prisma = new PrismaClient();
-
-export async function GET() {
-  const users = await prisma.user.findMany();
-  return NextResponse.json(users);
-}
---
+++ src/pages/api/users/[id].ts
@@ -0,0 +1,84 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'GET') {
+    const params = req.query;
+    const getAbsences = params.getAbsences === 'true';
+    const id = Number(params.id as string);
+
+    if (Number.isNaN(id)) {
+      return res.status(400).json({ error: 'Invalid ID provided' });
+    }
+
+    try {
+      const user = await prisma.user.findUnique({
+        where: { id },
+        include: { absences: getAbsences },
+      });
+
+      if (!user) {
+        return res.status(400).json({ error: 'User not found' });
+      }
+
+      return res.status(200).json(user);
+    } catch (error) {
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  } else if (req.method === 'PATCH') {
+    const data = req.body;
+    const id = Number(req.query.id as string);
+
+    if (Number.isNaN(id)) {
+      return res.status(400).json({ error: 'Invalid ID provided' });
+    }
+
+    try {
+      const updatedUser = await prisma.user.update({
+        where: { id },
+        data: {
+          email: data.email,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          role: data.role,
+          status: data.status,
+        },
+      });
+
+      if (!updatedUser) {
+        return res.status(400).json({ error: 'User not found' });
+      }
+
+      return res.status(200).json(updatedUser);
+    } catch (error) {
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  } else if (req.method === 'DELETE') {
+    const id = Number(req.query.id as string);
+
+    if (Number.isNaN(id)) {
+      return res.status(400).json({ error: 'Invalid ID provided' });
+    }
+
+    try {
+      const deletedUser = await prisma.user.delete({
+        where: { id },
+      });
+
+      if (!deletedUser) {
+        return res.status(400).json({ error: 'User not found' });
+      }
+
+      return res.status(200).json({ message: `User with id ${id} deleted` });
+    } catch (error) {
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  } else {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+}
