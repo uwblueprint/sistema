666c33ba4c8471a1930e78080178542e044e843e
David Lu <151972620+ludavidca@users.noreply.github.com>
2024-10-22 20:12:44 -0400

    Update utils/prisma.ts
    Co-authored-by: Chinemerem <chinemeremchigbo@Outlook.com>

--- utils/prisma.ts
+++ utils/prisma.ts
@@ -1,7 +1,13 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
