b1e6ff4cd1e684d78c69f587c46441059b958ee4
Chinemerem <chinemeremchigbo@Outlook.com>
2024-11-03 22:39:39 -0800

    Use app password

--- src/pages/api/sendEmail.ts
+++ src/pages/api/sendEmail.ts
@@ -21,7 +21,9 @@ export default async function handler(
   // Create a transporter object
   try {
     const transporter = nodemailer.createTransport({
-      service: process.env.EMAIL_SERVICE,
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS, // app password
