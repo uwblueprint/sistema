75d442b7fc0ffd4d90cded3554450e1d4dc04cf4
Chinemerem <chinemeremchigbo@Outlook.com>
2024-11-24 01:03:36 -0800

    Update seed.config.ts

--- prisma/seed/seed.config.ts
+++ prisma/seed/seed.config.ts
@@ -2,10 +2,20 @@ import { SeedPrisma } from '@snaplet/seed/adapter-prisma';
 import { defineConfig } from '@snaplet/seed/config';
 import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
+
+async function connectPrisma() {
+  await prisma.$connect();
+}
+
+connectPrisma().catch((e) => {
+  console.error('Failed to connect to the database:', e);
+  process.exit(1);
+});
+
 export default defineConfig({
   adapter: () => {
-    const client = new PrismaClient();
-    return new SeedPrisma(client);
+    return new SeedPrisma(prisma);
   },
   select: ['!*_prisma_migrations'],
 });
