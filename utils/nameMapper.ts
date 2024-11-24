4e4bdd14f7ccea30446ad72942afd400bb8fa018
Jason Ling <105073210+Jasonlingg@users.noreply.github.com>
2024-11-24 11:01:37 -0500

    added reusable mapper

---
+++ utils/nameMapper.ts
@@ -0,0 +1,29 @@
+export const nameMapping: Record<string, string> = {
+  'Parkdale Junior Senior Public School': 'Parkdale',
+  'Yorkwoods Public School': 'Yorkwoods',
+  'Lambton Park Community School': 'Lambton Park',
+  // Add other mappings here as needed
+};
+
+// Function to map names
+export const mapName = (originalName: string): string => {
+  return nameMapping[originalName] || originalName; // Return mapped name or fallback to original
+};
+export const transformName = (name: string): string => {
+  const nameMappings: Record<string, string> = {
+    STRINGS: 'Strings',
+    CHOIR: 'Choir',
+    M_AND_M: 'Music and Movement',
+    PERCUSSION: 'Percussion',
+    TRUMPET_AND_CLARINET: 'Trumpet/Clarinet',
+  };
+  return nameMappings[name] || name; // Fallback to the original name if no mapping exists
+};
+
+export const subjectProperties: Record<string, { color: string }> = {
+  STRINGS: { color: '#A761A3' }, // Primary Purple
+  CHOIR: { color: '#F6A957' }, // Primary Yellow
+  M_AND_M: { color: '#33B4A9' }, // Primary Green
+  PERCUSSION: { color: '#685777' }, // Primary Violet
+  TRUMPET_AND_CLARINET: { color: '#EC6A6B' }, // Primary Red
+};
