8d36f6e663d52222b94be30e3cea60c3227cecfd
Chinemerem <chinemeremchigbo@Outlook.com>
2024-11-25 02:48:34 -0800

    Add Poppins to fonts

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,7 +1,10 @@
 import { extendTheme } from '@chakra-ui/react';
 
-const fonts = { mono: `'Menlo', monospace` };
-
+const fonts = {
+  heading: `'Poppins', sans-serif`,
+  body: `'Poppins', sans-serif`,
+  mono: `'Menlo', monospace`,
+};
 const breakpoints = {
   sm: '40em',
   md: '52em',
