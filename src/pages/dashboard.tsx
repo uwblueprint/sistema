fe4d3028d894c10052b23ffe255b29b6ecc0e918
Chinemerem <chinemeremchigbo@Outlook.com>
2025-02-23 20:14:21 -0500

    Address Nits

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,14 @@
 import { Box } from '@chakra-ui/react';
 import DashboardHeader from '../components/DashboardHeader';
+import UserManagementSection from '../components/UserManagementSection';
 
-export default function Dashboard() {
+export default function DashboardPage() {
   return (
-    <Box bg="neutralGray.50" minH="100vh">
+    <Box>
       <DashboardHeader />
+      <Box px={8} pt={3} pb={8}>
+        <UserManagementSection />
+      </Box>
     </Box>
   );
 }
