3995d9f701342803b10e776ad9619f486a8bd5a9
Kenzy Soror <kenzy.soror.05@gmail.com>
2025-03-15 14:25:47 -0400

    Style lesson-plan view

--- src/components/LessonPlanView.tsx
+++ src/components/LessonPlanView.tsx
@@ -1,11 +1,49 @@
-import { Button, Link, Text, Flex, Image } from '@chakra-ui/react';
+import { Link, Text, Flex, Image, Box, IconButton } from '@chakra-ui/react';
+import { FiFileText } from 'react-icons/fi';
+import { VscArrowSwap } from 'react-icons/vsc';
 
-const LessonPlanView = ({ lessonPlan, absentTeacherFirstName }) => {
-  return lessonPlan ? (
-    <Button as={Link} href={lessonPlan} isExternal>
-      Download Lesson Plan (to-do)
-    </Button>
-  ) : (
+const LessonPlanDisplay = ({ href, fileName, fileSize }) => {
+  return (
+    <Flex
+      width="100%"
+      height="48px"
+      borderColor="#D2D2D2"
+      borderWidth="1px"
+      borderRadius="10px"
+      p={'16px'}
+      align="center"
+      bg="#FDFDFD"
+      justify="space-between"
+    >
+      <Link href={href} isExternal>
+        <Flex align="center">
+          <FiFileText size="24px" color="#0468C1" />
+          <Box ml="12px">
+            <Text fontSize="12px" color="#373636">
+              {fileName}
+            </Text>
+            {fileSize && (
+              <Text fontSize="11px" color="#838383">
+                {fileSize}
+              </Text>
+            )}
+          </Box>
+        </Flex>
+      </Link>
+
+      <IconButton
+        aria-label="Swap Lesson Plan"
+        icon={<VscArrowSwap size="15px" />}
+        size="5px"
+        variant="ghost"
+        color="#656565"
+      />
+    </Flex>
+  );
+};
+
+const NoLessonPlanFillingDisplay = ({ absentTeacherFirstName }) => {
+  return (
     <Flex
       align="center"
       justify="center"
@@ -29,6 +67,33 @@ const LessonPlanView = ({ lessonPlan, absentTeacherFirstName }) => {
         yet.
       </Text>
     </Flex>
+  );
+};
+
+const NoLessonPlanDeclaredDisplay = ({}) => {
+  return (
+    <Flex>
+      <Text fontSize="11px" color="#8C8C8C" width="154px">
+        TO-DO: Upload lesson plan + attach to absence
+      </Text>
+    </Flex>
+  );
+};
+
+const LessonPlanView = ({ lessonPlan, absentTeacherFirstName }) => {
+  const getFileName = (url) => (url ? 'File name: TO-DO' : '');
+  const getFileSize = (url) => (url ? 'File size: TO-DO' : '');
+
+  return lessonPlan ? (
+    <LessonPlanDisplay
+      href={lessonPlan}
+      fileName={getFileName(lessonPlan)}
+      fileSize={getFileSize(lessonPlan)}
+    />
+  ) : (
+    <NoLessonPlanFillingDisplay
+      absentTeacherFirstName={absentTeacherFirstName}
+    />
   );
 };
 
