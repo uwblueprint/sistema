765d67cd26771a93cc0542e880647817a77c58e1
Chinemerem <chinemeremchigbo@Outlook.com>
2025-03-25 15:05:11 -0400

    Make YearDropdown Match Figma

--- src/components/YearDropdown.tsx
+++ src/components/YearDropdown.tsx
@@ -10,9 +10,14 @@ import {
   InputRightElement,
   useDisclosure,
   Text,
+  Button,
+  Flex,
+  useTheme,
+  Divider,
 } from '@chakra-ui/react';
 import { IoChevronDown, IoChevronUp } from 'react-icons/io5';
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
+import { CalendarIcon } from './CalendarIcon';
 
 interface YearDropdownProps {
   selectedRange: string;
@@ -25,6 +30,7 @@ export default function YearDropdown({
   onChange,
   yearRanges,
 }: YearDropdownProps) {
+  const theme = useTheme();
   const { isOpen, onOpen, onClose } = useDisclosure();
   const inputRef = useRef<HTMLInputElement | null>(null);
 
@@ -43,37 +49,64 @@ export default function YearDropdown({
       onClose={onClose}
     >
       <PopoverTrigger>
-        <InputGroup>
-          <Input
-            ref={inputRef}
-            cursor="pointer"
-            textAlign="left"
-            pr="2.5rem"
-            placeholder="Select Year"
-            value={selectedRange}
-            readOnly
-          />
-          <InputRightElement pointerEvents="none">
-            {isOpen ? <IoChevronUp /> : <IoChevronDown />}
-          </InputRightElement>
-        </InputGroup>
+        <Button
+          bg={theme.colors.buttonBackground}
+          border="1px solid"
+          borderColor={theme.colors.neutralGray[300]}
+          borderRadius="7px"
+          py="11px"
+          px="15px"
+          height="40px"
+          width="207px"
+          maxWidth="207px"
+          _hover={{ bg: theme.colors.primaryBlue[50] }}
+          _active={{ bg: theme.colors.primaryBlue[50] }}
+        >
+          <Flex alignItems="center" justifyContent="space-between" width="100%">
+            <CalendarIcon />
+            <Flex flex="1" justifyContent="center">
+              <Text fontWeight="600" fontSize="16px" color="black">
+                {selectedRange}
+              </Text>
+            </Flex>
+            {isOpen ? (
+              <IoChevronUp size={21} color="black" />
+            ) : (
+              <IoChevronDown size={21} color="black" />
+            )}
+          </Flex>
+        </Button>
       </PopoverTrigger>
 
-      <PopoverContent width="200px" borderRadius="md" overflow="hidden">
+      <PopoverContent
+        width="207px"
+        borderRadius="md"
+        overflow="hidden"
+        border="1px solid"
+        borderColor={theme.colors.neutralGray[300]}
+      >
         <PopoverArrow />
         <PopoverBody p={0}>
-          {yearRanges.map((year) => (
-            <Box
-              key={year}
-              onClick={() => handleSelectYear(year)}
-              sx={{
-                padding: '10px 16px',
-                cursor: 'pointer',
-                bg: 'transparent',
-                _hover: { bg: 'neutralGray.100' },
-              }}
-            >
-              <Text textStyle="subtitle">{year}</Text>
+          {yearRanges.map((year, index) => (
+            <Box key={year}>
+              <Box
+                onClick={() => handleSelectYear(year)}
+                sx={{
+                  padding: '16px',
+                  cursor: 'pointer',
+                  bg: selectedRange === year ? 'primaryBlue.50' : 'transparent',
+                  _hover: { bg: 'neutralGray.100' },
+                  _active: { bg: 'neutralGray.300' },
+                  textAlign: 'left',
+                }}
+              >
+                <Text fontStyle="body" fontWeight="500">
+                  {year}
+                </Text>
+              </Box>
+              {index < yearRanges.length - 1 && (
+                <Divider borderColor="neutralGray.300" opacity={1} />
+              )}
             </Box>
           ))}
         </PopoverBody>
