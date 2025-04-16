0def7971c5981c67fa079af6459be49f58350e4e
Chinemerem <chinemeremchigbo@Outlook.com>
2025-04-16 04:24:07 -0400

    Update components.ts

--- theme/components.ts
+++ theme/components.ts
@@ -96,21 +96,29 @@ export const components = {
       },
     },
   },
-  borderColor: 'outline',
-
-  Tooltip: {
+  Textarea: {
     baseStyle: {
-      bg: 'white',
-      color: 'text.body',
-      borderRadius: 'md',
-      px: 3,
-      py: 2,
-      fontSize: 'sm',
-      maxWidth: '320px',
-      '--popper-arrow-bg': 'white',
-      boxShadow: 'md',
       border: '1px solid',
-      borderColor: 'neutralGray.200',
+      borderColor: 'outline',
+      borderRadius: '10px',
+      fontSize: '13px',
+      fontWeight: 400,
+      lineHeight: '15.73px',
+      fontFamily: 'body',
+      _placeholder: {
+        color: 'text.subtitle',
+      },
+      _hover: {
+        borderColor: 'outline',
+      },
+      _focus: {
+        borderColor: 'primaryBlue.300',
+        boxShadow: '0 0 0 1px primaryBlue.300',
+      },
+      _active: {
+        borderColor: 'primaryBlue.300',
+        boxShadow: '0 0 0 1px primaryBlue.300',
+      },
     },
   },
   Tooltip: {
